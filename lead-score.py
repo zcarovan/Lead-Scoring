c65d31e3260cdcf67e7536d30a0512c60d00e638
Zoe Carovano <63746631+zcarovan@users.noreply.github.com>
2024-11-12 14:56:55 -0500

    Lead scoring now tested with 6 attributes

--- lead-score.py
+++ lead-score.py
@@ -45,8 +45,8 @@ def create_percent_table(count_table):
     total_closed_count = count_table['Closed Deal'].sum()
     total_nonclosed_count = count_table['Non-closed Deal'].sum()
 
-    print("closed deal count: ", total_closed_count)
-    print("non-closed deal count: ", total_nonclosed_count)
+    #print("closed deal count: ", total_closed_count)
+    #print("non-closed deal count: ", total_nonclosed_count)
 
     percent_table = count_table.copy(deep=True)
 
@@ -89,37 +89,60 @@ goal is for range to be 100
 x = 100/ score_range
 - x is the score_scaler
 score_scaler is applied to the lead score to normalize it on a scale from 
-0-100, perfect score gets 100, worst score gets 0"""
+0-100, perfect score gets 100, worst score gets 0
+
+how to handle nulls?
+
+put in a 50 percent chance by default
+how to find a 50 percent chance weight?
+sum up all the weights in that attribute's weight table.
+divide by the number of weights to get the average.
+input that as the weight.
+
+"""
 def find_score_range(att_weight_tables):
     perfect_score = 0
     worst_score = 0
+    
     for att_weight in att_weight_tables:
         att_max = att_weight[1]['Difference'].max()
         att_min = att_weight[1]['Difference'].min()
         perfect_score = perfect_score + att_max
         worst_score = worst_score + att_min
-    print(perfect_score)
-    print(worst_score)
+    #print(perfect_score)
+    #print(worst_score)
     score_range = perfect_score - worst_score
     score_scaler = 100 / score_range
 
     return (score_scaler, worst_score)
 
+def average_weights(weight_table):
+    return
+
 def find_lead_score(lead, att_weight_tables, score_scaler, worst_score):
     # Go through single row, add up score.
     lead_score = 0
+    null_table = lead.isnull()
+    #print(null_table)
+
     for att_weight in att_weight_tables:
         # Get value for current attribute (ex/ "High Equity")
         lead_att_val = lead[att_weight[0]]
-        # Look up current attribute value in the weight table.
         curr_att_weight_table = att_weight[1]
-        print(curr_att_weight_table)
-        att_val_weight = curr_att_weight_table.loc[lead_att_val, 'Difference']
+        # If null:
+        if null_table[att_weight[0]]:
+            #Take the average of all the weights and use that as the weight.
+            att_val_weight = curr_att_weight_table['Difference'].mean()
+            #print(curr_att_weight_table)
+            #print("average weight is: ", att_val_weight)
+        #If value is present, use the specified weight.
+        else:
+            att_val_weight = curr_att_weight_table.loc[lead_att_val, 'Difference']
         # Add attribute value's weight to lead score.
         lead_score = lead_score + att_val_weight
     
     lead_score = lead_score - worst_score
-    print(lead_score)
+    #print(lead_score)
     lead_score = lead_score * score_scaler
     return lead_score
 
@@ -129,37 +152,39 @@ def main():
     lead_data = pd.read_excel('trial4.xlsx')
 
     # Pick a lead attribute to collect data on.
-    attributes = ['Equity', 'Property Condition']
+    attributes = ['Equity', 'Property Condition', 'Occupancy', 'How Soon Would You Like to Settle?', 'Property Type', 'Assigned  To - Ac Manager - name']
     att_weight_tables = []
     # Go through all attributes in attribute list. Find weights for each value in attributes' respective dataframe.
     for att in attributes:
         # Count how many closed and nonclosed deals are associated with each attribute value.
         count_table = count_attribute_with_deal(lead_data, att)
-        print(count_table)
+        #print(count_table)
 
         # Create a table of the percentage times of each permutation between deal 
         # status and the attribute.
         percent_table = create_percent_table(count_table)
 
         weight_table = create_weight_table(percent_table)
-        print(weight_table)
+        #print("weight_table: ", weight_table)
         att_weight_tables.append([att, weight_table])
 
-    print(att_weight_tables)
+    #print(att_weight_tables)
 
     #Add 100
     for att_weight in att_weight_tables:
         scale_weights(att_weight[1])
 
-    print(att_weight_tables)
+    #print(att_weight_tables)
     
     (score_scaler, worst_score) = find_score_range(att_weight_tables)
-    print("score scaler: ", score_scaler)
+    #print("score scaler: ", score_scaler)
 
-    first_lead = lead_data.iloc[1]
-    print(first_lead)
-    lead_score = find_lead_score(first_lead, att_weight_tables, score_scaler, worst_score)
-    print(lead_score)
+    curr_lead = lead_data.iloc[210]
+    print("The lead we are analyzing:")
+    print(curr_lead)
+    lead_score = find_lead_score(curr_lead, att_weight_tables, score_scaler, worst_score)
+    rounded_lead_score = round(lead_score)
+    print("Lead has a", rounded_lead_score, "percent chance of resulting in closed deal.")
 
     # Display the percent table as a bar graph.
     #create_bar_graph(percent_table, attribute_name)
